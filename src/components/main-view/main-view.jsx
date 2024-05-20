e7e05f8b6db739c0b78aa507accf209897fe7694
Vinnie <vincentathorne2005@gmail.com>
2024-05-20 11:50:06 -0700

    removing fluff

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -28,54 +28,7 @@ export const MainView = () => {
       });
   }, [token]);
 
-  //   if (!user) {
-  //     return (
-  //       <>
-  //         <LoginView
-  //           onLoggedIn={(user, token) => {
-  //             setUser(user);
-  //             setToken(token);
-  //           }}
-  //         />
-  //         or
-  //         <SignupView />
-  //       </>
-  //     );
-  //   }
-  //   if (selectedMovie) {
-  //     return (
-  //       <MovieView
-  //         movie={selectedMovie}
-  //         onBackClick={() => setSelectedMovie(null)}
-  //       />
-  //     );
-  //   }
-  //   if (movies.length === 0) {
-  //     return <div>The list is empty!</div>;
-  //   }
-  //   return (
-  //     <div>
-  //       <button
-  //         onClick={() => {
-  //           setUser(null);
-  //           setToken(null);
-  //           localStorage.clear();
-  //         }}
-  //       >
-  //         Logout
-  //       </button>
-  //       {movies.map((movie) => (
-  //         <MovieCard
-  //           key={movie.id}
-  //           movie={movie}
-  //           onMovieClick={(newSelectedMovie) => {
-  //             setSelectedMovie(newSelectedMovie);
-  //           }}
-  //         />
-  //       ))}
-  //     </div>
-  //   );
-  // };
+
 
   return (
     <Row className="justify-content-md-center">
